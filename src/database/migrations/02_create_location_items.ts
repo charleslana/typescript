36b7cdd271716285347ad5b94304d8c93626d67d
Charles Lana <charleslanop@gmail.com>
2020-12-12 20:28:40 -0300

    Commit 17

--- src/database/migrations/02_create_location_items.ts
+++ src/database/migrations/02_create_location_items.ts
@@ -3,11 +3,11 @@ import Knex from 'knex';
 export async function up(knex: Knex) {
     return knex.schema.createTable('location_items', table => {
         table.increments('id').primary();
-        table.string('location_id')
+        table.integer('location_id')
             .notNullable()
             .references('id')
             .inTable('locations');
-        table.string('item_id')
+        table.integer('item_id')
             .notNullable()
             .references('id')
             .inTable('items');
