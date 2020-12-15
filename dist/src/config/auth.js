718159172608dd20598e57cbdd9f9f3285f2a324
Charles Lana <charleslanop@gmail.com>
2020-12-14 22:58:12 -0300

    Commit 27

--- dist/src/config/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    jwt: {
-        secret: '50471ca11abd89dbcb646533f43f39b6',
-        expiresIn: '1d'
-    }
-};
